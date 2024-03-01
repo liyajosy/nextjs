a347fcf37ae541ab63cf1ff334910a123ab6f3a4
liya <liyajosy@gmail.com>
2024-03-01 16:42:06 -0500

    updated config

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,12 +16,8 @@ const Navbar = ()=>{
                 'bg-sky-100 text-blue-600': pathname === "/",
               },
             )} >Homepage</Link>
-            <Link href ="/about" className={clsx(
-              'justify-center gap-2 rounded-md bg-gray-50 p-3 ',
-              {
-                'bg-sky-100 text-blue-600': pathname === "/about",
-              },
-            )}>About</Link>
+            <Link href ="/about" className={clsx('justify-center gap-2 rounded-md bg-gray-50 p-3 ', { 'bg-sky-100 text-blue-600': pathname === "/about",}, )}>
+                About Page</Link>
             <Link href ="/contact"className={clsx(
               'justify-center gap-2 rounded-md bg-gray-50 p-3 ',
               {
