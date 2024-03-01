0796746b16e4af9965228f4c32480a9a3dce5469
liya <liyajosy@gmail.com>
2024-03-01 16:03:20 -0500

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+
+
+const nextConfig = {
+    basePath: "/next-app",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
