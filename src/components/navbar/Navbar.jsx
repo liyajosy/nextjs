06bb10b94b98da3b8054c12158864f39d71384bf
liya <liyajosy@gmail.com>
2024-03-01 15:27:26 -0500

    first next js commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Link from "next/link"
+import styles from "@/mystyles.module.css";
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
+
+const Navbar = ()=>{
+    const pathname= usePathname();
+    return (
+        <>
+            <div className={ styles.mystyle}> 
+            <Link href ="/" className={clsx(
+              'justify-center gap-2 rounded-md bg-gray-50 p-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === "/",
+              },
+            )} >Homepage</Link>
+            <Link href ="/about" className={clsx(
+              'justify-center gap-2 rounded-md bg-gray-50 p-3 ',
+              {
+                'bg-sky-100 text-blue-600': pathname === "/about",
+              },
+            )}>About</Link>
+            <Link href ="/contact"className={clsx(
+              'justify-center gap-2 rounded-md bg-gray-50 p-3 ',
+              {
+                'bg-sky-100 text-blue-600': pathname === "/contact",
+              },
+            )}>Contact</Link>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
